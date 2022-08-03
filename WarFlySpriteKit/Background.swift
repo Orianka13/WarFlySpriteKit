8776767e71be3e711c7a6914d42710bc5ce1f105
Orianka13 <odobrogor@gmail.com>
2022-08-03 15:42:53 +0300

    Добавила задний фон

---
+++ WarFlySpriteKit/Background.swift
@@ -0,0 +1,20 @@
+//
+//  Background.swift
+//  WarFlySpriteKit
+//
+//  Created by Олеся Егорова on 03.08.2022.
+//
+
+import UIKit
+import SpriteKit
+
+class Background: SKSpriteNode {
+
+    static func populateBackground(at point: CGPoint) -> Background {
+        let background = Background(imageNamed: "background")
+        background.position = point
+        background.zPosition = 0 // чтобы задний фон не перекрывал остальные объекты
+        
+        return background
+    }
+}

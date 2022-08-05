b0cc2d36eccaec0e0c6cf1a4b4013dc2d288da80
Orianka13 <odobrogor@gmail.com>
2022-08-05 11:30:40 +0300

    Вынесла протокол в отдельный файл

---
+++ WarFlySpriteKit/GameBackgoudSpritable + extension.swift
@@ -0,0 +1,28 @@
+//
+//  GameBackgoudSpriteable + extension.swift
+//  WarFlySpriteKit
+//
+//  Created by Олеся Егорова on 05.08.2022.
+//
+
+import SpriteKit
+import GameplayKit
+
+
+protocol GameBackgroundSpritable {
+    static func populate() -> Self // Self - означает что должен быть возвращен либо тип протокола, либо тип класса, который подписан под этот протокол - у класса поставить final
+    static func randomPoint() -> CGPoint
+}
+
+
+extension GameBackgroundSpritable {
+    //сгенерируем точку от 100 до 200 выше экрана (для генерации островов и облаков ЗА пределами экрана)
+    static func randomPoint() -> CGPoint {
+        let screen = UIScreen.main.bounds
+        let distribution = GKRandomDistribution(lowestValue: Int(screen.size.height) + 100, highestValue: Int(screen.size.height) + 200)
+        let y = CGFloat(distribution.nextInt()) //nextInt() - используется для генерации значений
+        let x = CGFloat(GKRandomSource.sharedRandom().nextInt(upperBound: Int(screen.size.width)))
+        
+        return CGPoint(x: x, y: y)
+    }
+}
--- WarFlySpriteKit/Sprites/Cloud.swift
+++ WarFlySpriteKit/Sprites/Cloud.swift
@@ -9,23 +9,6 @@ import UIKit
 import SpriteKit
 import GameplayKit
 
-protocol GameBackgroundSpritable {
-    static func populate() -> Self // Self - означает что должен быть возвращен либо тип протокола, либо тип класса, который подписан под этот протокол - у класса поставить final
-    static func randomPoint() -> CGPoint
-}
-
-
-extension GameBackgroundSpritable {
-    //сгенерируем точку от 100 до 200 выше экрана (для генерации островов и облаков ЗА пределами экрана)
-    static func randomPoint() -> CGPoint {
-        let screen = UIScreen.main.bounds
-        let distribution = GKRandomDistribution(lowestValue: Int(screen.size.height) + 100, highestValue: Int(screen.size.height) + 200)
-        let y = CGFloat(distribution.nextInt()) //nextInt() - используется для генерации значений
-        let x = CGFloat(GKRandomSource.sharedRandom().nextInt(upperBound: Int(screen.size.width)))
-        
-        return CGPoint(x: x, y: y)
-    }
-}
 
 final class Cloud: SKSpriteNode, GameBackgroundSpritable {
     // создаем облако

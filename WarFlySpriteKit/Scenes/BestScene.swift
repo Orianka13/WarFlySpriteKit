ea13d8ce9bf19de4e16f2fcbc923c2c63c5bf999
Orianka13 <odobrogor@gmail.com>
2022-08-30 16:34:36 +0300

    Доработана bestScene
    добавлен топ 3

--- WarFlySpriteKit/Scenes/BestScene.swift
+++ WarFlySpriteKit/Scenes/BestScene.swift
@@ -8,19 +8,28 @@
 import SpriteKit
 
 class BestScene: ParentScene {
+    
+    private var places = [10, 100, 10000]
+    
     override func didMove(to view: SKView) {
         
         setHeader(withName: "best", andBackground: "header_background")
         
-        let score = getMenuButton(withName: "score", title: "10 467",
-                                  location: CGPoint(x: self.frame.midX, y: self.frame.midY),
-                                  backgroundName: "button_background")
-        addChild(score)
-        
         let back = getMenuButton(withName: "back", title: "back",
-                                 location: CGPoint(x: self.frame.midX, y: self.frame.midY - 100),
+                                 location: CGPoint(x: self.frame.midX, y: self.frame.midY - 200),
                                  backgroundName: "button_background")
         addChild(back)
+        
+        let topPlaces = self.places.sorted { $0 > $1 }.prefix(3)
+        
+        for (index, value) in topPlaces.enumerated() {
+            let l = SKLabelNode(text: value.description)
+            l.fontColor = UIColor(red: 219 / 255, green: 226 / 255, blue: 215 / 255, alpha: 1.0)
+            l.fontName = "AmericanTypewriter-Bold"
+            l.fontSize = 30
+            l.position = CGPoint(x: self.frame.midX, y: self.frame.midY - CGFloat(index * 60))
+            addChild(l)
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
